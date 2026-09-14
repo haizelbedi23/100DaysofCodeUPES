f468c9e63abe851cb5969d7dbd8e5181a9a39054
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:26:58 +0530

    Add Day 28/Q56.c

---
+++ Day 28/Q56.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Array: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
