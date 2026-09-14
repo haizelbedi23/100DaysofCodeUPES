a7a23b494d496b87044dd3f3be5da43f8ab8abfd
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:25:28 +0530

    Add Day 11/Q22.c

---
+++ Day 11/Q22.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float cp, sp, profit, loss, percent;
+
+    printf("Enter cost price and selling price: ");
+    scanf("%f %f", &cp, &sp);
+
+    if (sp > cp) {
+        profit = sp - cp;
+        percent = (profit / cp) * 100;
+        printf("Profit %.2f%%", percent);
+    } else if (cp > sp) {
+        loss = cp - sp;
+        percent = (loss / cp) * 100;
+        printf("Loss %.2f%%", percent);
+    } else {
+        printf("No Profit No Loss");
+    }
+
+    return 0;
+}
