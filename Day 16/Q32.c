ca44ef3097cc7a7941c92850b6896fc6da22c5d6
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:25:56 +0530

    Add Day 16/Q32.c

---
+++ Day 16/Q32.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, original, digit, reverse = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    original = n;
+
+    while (n != 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
+
+    return 0;
+}
