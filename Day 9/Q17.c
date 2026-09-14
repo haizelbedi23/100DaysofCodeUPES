4d3d5ac749244051ef418b503fbbb011c8906300
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:25:04 +0530

    Add Day 9 Q17 solution

---
+++ Day 9/Q17.c
@@ -0,0 +1,19 @@
+// Q17: Find and categorize roots of a quadratic equation.
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float a, b, c, d, r1, r2;
+    scanf("%f %f %f", &a, &b, &c);
+    d = b*b - 4*a*c;
+    if (d > 0) {
+        r1 = (-b + sqrt(d)) / (2*a);
+        r2 = (-b - sqrt(d)) / (2*a);
+        printf("Roots are real and different: %.2f, %.2f", r1, r2);
+    } else if (d == 0) {
+        r1 = -b / (2*a);
+        printf("Roots are real and same: %.2f", r1);
+    } else {
+        printf("Roots are complex");
+    }
+    return 0;
+}
