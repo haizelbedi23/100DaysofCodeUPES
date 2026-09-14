ee341b71f34763782fa28583caa55c78e9a6f882
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:24:56 +0530

    Add Day 7 Q13 solution

---
+++ Day 7/Q13.c
@@ -0,0 +1,11 @@
+// Q13: Check whether a year is a leap year.
+#include <stdio.h>
+int main() {
+    int year;
+    scanf("%d", &year);
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        printf("Leap year");
+    else
+        printf("Not a leap year");
+    return 0;
+}
