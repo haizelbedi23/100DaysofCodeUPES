6ca7a450b8a6ad214547ecf075a4e85a601cb068
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:25:06 +0530

    Add Day 9 Q18 solution

---
+++ Day 9/Q18.c
@@ -0,0 +1,12 @@
+// Q18: Assign a grade from a percentage.
+#include <stdio.h>
+int main() {
+    int p;
+    scanf("%d", &p);
+    if (p >= 90 && p <= 100) printf("Grade A");
+    else if (p >= 80) printf("Grade B");
+    else if (p >= 70) printf("Grade C");
+    else if (p >= 60) printf("Grade D");
+    else printf("Grade F");
+    return 0;
+}
