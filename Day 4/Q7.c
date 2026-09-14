961399b1c79fa4bb79824212ec245c3888ea2f99
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:24:44 +0530

    Add Day 4 Q7 solution

---
+++ Day 4/Q7.c
@@ -0,0 +1,11 @@
+// Q7: Swap two numbers without using a third variable.
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swap: %d %d", a, b);
+    return 0;
+}
