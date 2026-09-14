f1f3298aad89585be7424f931b40716d0a8967c8
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:24:36 +0530

    Add Day 2 Q3 solution

---
+++ Day 2/Q3.c
@@ -0,0 +1,9 @@
+// Q3: Calculate area and perimeter of a rectangle.
+#include <stdio.h>
+int main() {
+    float length, breadth;
+    scanf("%f %f", &length, &breadth);
+    printf("Area = %.2f\n", length * breadth);
+    printf("Perimeter = %.2f", 2 * (length + breadth));
+    return 0;
+}
