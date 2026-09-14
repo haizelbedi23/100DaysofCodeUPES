3f38a5583497e95f49c44ce8286d1be478f4a75b
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:25:09 +0530

    Add Day 10 Q19 solution

---
+++ Day 10/Q19.c
@@ -0,0 +1,10 @@
+// Q19: Classify a triangle as equilateral, isosceles or scalene.
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a == b && b == c) printf("Equilateral");
+    else if (a == b || b == c || a == c) printf("Isosceles");
+    else printf("Scalene");
+    return 0;
+}
