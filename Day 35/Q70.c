bfb1b09396c6aac03c03c86ca3da07c9ee503bc9
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:28:43 +0530

    Add Day 35/Q70.c

---
+++ Day 35/Q70.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, k, i, j, temp;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) scanf("%d", &arr[i]);
+    printf("Enter k: ");
+    scanf("%d", &k);
+    k = k % n;
+    for (i = 1; i <= k; i++) {
+        temp = arr[n - 1];
+        for (j = n - 1; j > 0; j--) arr[j] = arr[j - 1];
+        arr[0] = temp;
+    }
+    printf("Rotated array: ");
+    for (i = 0; i < n; i++) printf("%d ", arr[i]);
+    return 0;
+}
