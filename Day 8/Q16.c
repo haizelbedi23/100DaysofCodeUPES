5ee639e4b465da6b0760d9611cac31a9eaa27d7c
Haizel Bedi <haizelbedi97@gmail.com>
2026-09-14 10:25:02 +0530

    Add Day 8 Q16 solution

---
+++ Day 8/Q16.c
@@ -0,0 +1,11 @@
+// Q16: Find the largest of three numbers.
+#include <stdio.h>
+int main() {
+    int a, b, c, largest;
+    scanf("%d %d %d", &a, &b, &c);
+    largest = a;
+    if (b > largest) largest = b;
+    if (c > largest) largest = c;
+    printf("Largest is %d", largest);
+    return 0;
+}
